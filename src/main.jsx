ab2cc282f3260ec2318a6d6012ba04878d0a8a77
vflorencia <florencia.vargas@goup.cloud>
2024-02-08 17:00:46 -0300

    cambio de back url

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import axios from 'axios';
 
 //Local
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://back-mynight-dcker.azurewebsites.net";
 
 //deploy
 // axios.defaults.baseURL = "URL BACK DEPLOYADO";
